233033672ea6d5c667dd3875e0bf7badf1285f70
Venkatesh Shanbhag <91714892+theshanbhag@users.noreply.github.com>
2023-06-15 20:09:58 +0530

    updated readme and added search for claims collection

--- chatter.py
+++ chatter.py
@@ -24,29 +24,28 @@ def predict_large_language_model_sample(
 
 
 def predict_large_language_model_sentiment(
-    project_id: str,
-    model_name: str,
-    temperature: float,
-    max_decode_steps: int,
-    top_p: float,
-    top_k: int,
-    content: str,
-    location: str = "us-central1",
-    tuned_model_name: str = "",
-    ) :
+        project_id: str,
+        model_name: str,
+        temperature: float,
+        max_decode_steps: int,
+        top_p: float,
+        top_k: int,
+        content: str,
+        location: str = "us-central1",
+        tuned_model_name: str = "",
+):
     """Predict using a Large Language Model."""
     vertexai.init(project=project_id, location=location)
     model = TextGenerationModel.from_pretrained(model_name)
     if tuned_model_name:
-      model = model.get_tuned_model(tuned_model_name)
+        model = model.get_tuned_model(tuned_model_name)
     response = model.predict(
         content,
         temperature=temperature,
         max_output_tokens=max_decode_steps,
         top_k=top_k,
-        top_p=top_p,)
+        top_p=top_p, )
     return response.text
-
 
 
 def chat_with_model(parameters, question, chat):
@@ -77,14 +76,22 @@ def upsert_db(client, db_name, col_name, doc, customer_id):
     col = db[col_name]
     col.update_one({"customer_id": customer_id}, {"$push": {"conversation": doc}})
 
+
 def insert_record(client, db_name, col_name, doc, customer_id):
     db = client[db_name]
     col = db[col_name]
-    col.insert_one({"customer_id":customer_id, "conversation": ""})
+    col.insert_one({"customer_id": customer_id, "conversation": ""})
 
-def search_mongoDB_index(client,question):
-    db = client["XYZ-Corp"]
-    col = db["chat-search"]
-    response = col.aggregate([{'$search':{'index': 'default','text': {'query': question, 'path':'question'}}}, {'$limit': 1}])
+
+def search_mongoDB_chat_base(db, col,client, question):
+    db = client[db]
+    col = db[col]
+    response = col.aggregate([{'$search': {'index': 'default', 'text': {'query': question, 'path': 'question'}}}, {'$limit': 1}])
     return response
 
+
+def search_mongoDB_claim(db, col,client, question):
+    db = client[db]
+    col = db[col]
+    response = col.aggregate([{'$search': {'index': 'default', 'text': {'query': question, 'path': 'claim_number'}}}, {'$limit': 1}])
+    return response
